b1bf58f6c40d90a52da71d252dfaa82296f41427
Shovaan83 <shovanbthr@gmail.com>
2026-03-28 10:24:37 +0545

    feat: add auto-migration on startup

--- backend/TalentVerse.WebAPI/Program.cs
+++ backend/TalentVerse.WebAPI/Program.cs
@@ -346,5 +346,11 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapHub<ChatHub>("/hubs/chat");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
 
